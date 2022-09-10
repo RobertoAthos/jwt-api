9b32a9235e12b140a233f673823c571884dd6785
RoebertoAthos <robertoathos22@gmail.com>
2022-09-10 11:17:16 -0300

    added cors on server

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const mongoose = require('mongoose')
 const app = express()
 const UserRouter = require('./Routes/UserRoutes')
 const AdminRoute = require('./Routes/AdminRoutes')
+const cors = require('cors')
 
 app.use('/user',express.json(),UserRouter)
 app.use('/admin', express.json(), AdminRoute)
+app.use(cors())
 
 const PORT = process.env.PORT || 5000
 
